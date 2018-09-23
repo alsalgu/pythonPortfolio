1e75474952d30c53831b9c7aef7c59d873ec02ed
Al <xochipillitx@gmail.com>
2018-09-23 15:58:34 -0500

    Configuration moved to config object class, Bootstrap added.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config, DebugConfig
 
-def create_app():
+def create_app(config_class=DebugConfig):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Felix'
+    app.config.from_object(config_class)
+    app.config['DEBUG'] = True
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
