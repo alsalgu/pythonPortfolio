2aa4d77027f0bd2a29858e16997b87f6f56ea186
Al <xochipillitx@gmail.com>
2018-09-25 01:07:17 -0500

    ms tool buttons change canvas accordingly functions

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -7,13 +7,19 @@ $(document).ready(function () {
     function resetButtons() {
         $("#MStoolbar").find(".embossBTN2").each(function () {
             $(this).removeClass("embossBTN2").addClass('embossBTN')
-        })
+        });
+        $('#MScanvas').find('.paint').each(function(){
+            $(this).addClass('d-none');
+        });
     };
 
     $("#MStoolbar").find(".eb").each(function () {
         $(this).on('click',function () {
+            var eID = '.' + $(this).attr('id');
             resetButtons();
             activeButton(this);
+            $(eID).removeClass('d-none');
+            console.log(eID)
         })
     });
 })
