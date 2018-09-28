8de7d93de9a3622ae30f8619791127ef44c1f27c
Al <xochipillitx@gmail.com>
2018-09-27 20:02:06 -0500

    Script that adds the active class to the proper link based on url

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -8,21 +8,37 @@ $(document).ready(function () {
         $("#MStoolbar").find(".embossBTN2").each(function () {
             $(this).removeClass("embossBTN2").addClass('embossBTN')
         });
-        $('#MSPaint').find('.paint').each(function(){
+        $('#MSPaint').find('.paint').each(function () {
             $(this).addClass('d-none');
         });
     };
 
-    $(function(){
+    $(function () {
         $('[data-toggle="tooltip"]').tooltip();
-      });
+    });
 
     $("#MStoolbar").find(".eb").each(function () {
-        $(this).on('click',function () {
+        $(this).on('click', function () {
             var eID = '.' + $(this).attr('id');
             resetButtons();
             activeButton(this);
             $(eID).removeClass('d-none');
         })
     });
+
+
+    var current_page = ""
+    var page_cat = ""
+
+    if (window.location.pathname != "/") {
+        var pathname = window.location.pathname.substring(1);
+        pathname = pathname.replace(/\//g, '-');
+        current_page = "#" + pathname;
+        page_cat = window.location.pathname.split('/')
+    } else {
+        current_page = "#home";
+    }
+
+    $(current_page).addClass('active');
+    $("#" + page_cat[1]).addClass('active');
 })
